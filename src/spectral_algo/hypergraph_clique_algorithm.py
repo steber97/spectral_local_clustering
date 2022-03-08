fd82c4192ebd8f1a9c357075b698fa57b72365b5
Stefano Huber <stefano.huber@gmail.com>
2022-03-08 11:41:16 +0100

    Hypergraph local clustering works with class solver

--- src/spectral_algo/hypergraph_clique_algorithm.py
+++ src/spectral_algo/hypergraph_clique_algorithm.py
@@ -6,38 +6,42 @@ import time
 from tqdm import tqdm
 
 
-def hypergraph_local_clustering_by_clique(hypergraph: HyperGraph, v: HyperNode, mu: float = 0.1):
-    """
-    """
-    # Turn hypergraph into a standard graph with clique transformation
-    nodes = []
-    edges = []
-    time_start = time.time()
-    for hn in sorted(hypergraph.hypernodes, key=lambda x: x.id):
-        nodes.append(Node(hn.id, hn.id))
-    nodes = np.array(nodes)
-    for i, he in enumerate(hypergraph.hyperedges):
-        for hn in he.hypernodes:
-            for hn2 in he.hypernodes:
-                if hn.id != hn2.id:
-                    edges.append(Edge(start=nodes[hn.id], end=nodes[hn2.id], weight=he.weight))
-    graph = Graph(nodes, edges)
-    print("Clique graph, nodes: {}, edges: {}, time to create: {}s".format(len(nodes), len(edges), time.time() - time_start))
-    
-    best_cut = None
-    best_conductance = None
-    for alpha in range(5, 100, 5):
-        # Center the probability in the starting vertex.
-        p_0 = np.zeros(len(nodes))
-        p_0[v.id] = 1.0
-        ppr = pagerank(graph.M, p_0, delta=1e-8, alpha=alpha/100)
-        # Take sweep on the ppr, as long as the cut is not larger than mu * vol(H)
-        cut = hypergraph.compute_lovasz_simonovits_sweep(ppr, mu=mu)
-        conductance = hypergraph.compute_conductance(cut)
-        if best_conductance is None or conductance < best_conductance:
-            best_conductance = conductance
-            best_cut = cut
-    
-    return best_cut
+class HyperGraphLocalClusteringByClique:
+
+    def __init__(self, hypergraph: HyperGraph):
+        # Turn hypergraph into a standard graph with clique transformation
+        nodes = []
+        edges = []
+        time_start = time.time()
+        for hn in sorted(hypergraph.hypernodes, key=lambda x: x.id):
+            nodes.append(Node(hn.id, hn.id))
+        nodes = np.array(nodes)
+        for i, he in enumerate(hypergraph.hyperedges):
+            for hn in he.hypernodes:
+                for hn2 in he.hypernodes:
+                    if hn.id != hn2.id:
+                        edges.append(Edge(start=nodes[hn.id], end=nodes[hn2.id], weight=he.weight))
+        self.graph = Graph(nodes, edges)
+        print("Clique graph, nodes: {}, edges: {}, time to create: {}s".format(len(nodes), len(edges), time.time() - time_start))
+
+    def hypergraph_local_clustering_by_clique(self, hypergraph: HyperGraph, v: HyperNode, mu: float = 0.1):
+        """
+
+        """
+        best_cut = None
+        best_conductance = None
+        for alpha in range(5, 100, 5):
+            # Center the probability in the starting vertex.
+            p_0 = np.zeros(len(hypergraph.hypernodes))
+            p_0[v.id] = 1.0
+            ppr = pagerank(self.graph.M, p_0, delta=1e-8, alpha=alpha/100)
+            # Take sweep on the ppr, as long as the cut is not larger than mu * vol(H)
+            cut = hypergraph.compute_lovasz_simonovits_sweep(ppr, mu=mu)
+            conductance = hypergraph.compute_conductance(cut)
+            if best_conductance is None or conductance < best_conductance:
+                best_conductance = conductance
+                best_cut = cut
+        
+        return best_cut
 
 
--- src/spectral_algo/hypergraph_local_clustering.py
+++ src/spectral_algo/hypergraph_local_clustering.py
@@ -1,27 +1,35 @@
+from ast import arg
 from datasets.hypergraphs.d_regular_r_uniform.read_graph import read_graph
-from src.spectral_algo.hypergraph_clique_algorithm import hypergraph_local_clustering_by_clique
+from src.spectral_algo.hypergraph_clique_algorithm import HyperGraphLocalClusteringByClique
 from src.spectral_algo.input_loader import input_loader_hypergraph
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import time
+import argparse
 
+args = argparse.ArgumentParser("Perform local graph clustering algorithm")
+args.add_argument("--dataset", type=str, choices=[
+    'network_theory', 'opsahl_collaboration'
+])
+args = args.parse_args()
 
 if __name__ == "__main__":
     start_time = time.time()
     # hypergraph = read_graph("datasets/hypergraphs/d_regular_r_uniform/n_6_d_2_r_4.txt")
     # hypergraph = read_graph("datasets/hypergraphs/d_regular_r_uniform/n_400_d_10_r_8.txt")
-    # hypergraph = input_loader_hypergraph("network_theory")
-    hypergraph = input_loader_hypergraph("opsahl_collaboration")
+    hypergraph = input_loader_hypergraph(args.dataset)
+
     print("Input taken in {}s".format(time.time() - start_time))
     mu = 0.1
     conductances = []
+    local_clustering_clique = HyperGraphLocalClusteringByClique(hypergraph=hypergraph)
     for i in tqdm(range(50)):
         # Take a random vertex.
         v = hypergraph.hypernodes[np.random.randint(0, len(hypergraph.hypernodes))]
-        cut = hypergraph_local_clustering_by_clique(hypergraph, v, mu)
+        cut = local_clustering_clique.hypergraph_local_clustering_by_clique(hypergraph, v, mu)
         conductance = hypergraph.compute_conductance(cut)
         conductances.append(conductance)
-    print(conductances)
+    
     plt.plot(range(len(conductances)), sorted(conductances))
     plt.show()
