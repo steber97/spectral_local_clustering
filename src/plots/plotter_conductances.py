8a573baf7acf2b4f09bcfe9f00064497276b1ae3
Stefano Huber <stefano.huber@gmail.com>
2022-03-17 12:35:38 +0100

    Beautify plots, include time boxplots

--- src/plots/plotter_conductances.py
+++ src/plots/plotter_conductances.py
@@ -11,23 +11,33 @@ args = args.parse_args()
 if __name__ == "__main__":
     df = pd.read_csv(args.csv_file)
     fig, axes = plt.subplots(1, 2)
+    methods = sorted(list(set([label.replace("_conductance", "").replace("_time", "") for label in df.columns])))
+    running_time_per_method = {}
     for label in df.columns:
-        x_s = []
-        y_s = []
-        conductances_sorted = sorted(df[label])
-        x_s.append(0)
-        y_s.append(conductances_sorted[0])
-        for i in range(1, len(conductances_sorted)):
-            x_s.append(i)
-            y_s.append(conductances_sorted[i-1])
-            x_s.append(i)
-            y_s.append(conductances_sorted[i])
-            
-        axes[0].plot(x_s, y_s, label=label)
-    
-    # axes[1].boxplot([running_time_per_label[label] for label in labels], labels=labels)
+        if "_conductance" in label:
+            x_s = []
+            y_s = []
+            conductances_sorted = sorted(df[label])
+            x_s.append(0)
+            y_s.append(conductances_sorted[0])
+            for i in range(1, len(conductances_sorted)):
+                x_s.append(i)
+                y_s.append(conductances_sorted[i-1])
+                x_s.append(i)
+                y_s.append(conductances_sorted[i])
+                
+            axes[0].plot(x_s, y_s, label=label.replace("_conductance", ""))
+        elif "_time" in label:
+            method = label.split("_time")[0]
+            running_time_per_method[method] = df[label]
+    axes[1].boxplot([running_time_per_method[method] for method in methods], 
+                    labels=methods)
+    plt.xticks(rotation=45)
 
     axes[0].set_ylim(0)
     axes[0].legend()
-    # axes[1].legend()
+    axes[1].legend()
+    plt.suptitle(args.csv_file.replace(".csv", "").\
+        replace("Hypergraph_clustering_based_on_PageRank/output/output_conductances_",""))
+    plt.subplots_adjust()
     plt.show()
