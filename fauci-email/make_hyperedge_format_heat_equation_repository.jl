9a3af8c27351741d94974ee859a038a9593c13a6
Stefano Huber <stefano.huber@gmail.com>
2022-05-10 11:17:06 +0200

    Add hypergraph for non_cc and cc fauci emails

--- fauci-email/make_hyperedge_format_heat_equation_repository.jl
+++ fauci-email/make_hyperedge_format_heat_equation_repository.jl
@@ -1,14 +1,32 @@
 include("methods.jl")
 include("methods_hypergraph.jl")
 
-kf = false
-ms = 100
-mindeg = 5
-parts=("sender","recipients")
-H = _build_email_hypergraph(data;hyperedgeparts=parts,maxset=ms, keepfauci=kf,mindegree = mindeg)
 
-open("../Hypergraph_clustering_based_on_PageRank/SubmodularHeatEquation/instance/fauci_email.txt", "w") do f
-    for (edge, weight) in zip(H.elist, H.weights)
+# Create dataset without CC
+kf = true  # Keep fauci
+mindeg = 2
+parts_no_cc = ("sender","recipients")
+H_no_cc = _build_email_hypergraph(data; hyperedgeparts=parts_no_cc, keepfauci=kf,  mindegree = mindeg)
+
+open("../Hypergraph_clustering_based_on_PageRank/SubmodularHeatEquation/instance/fauci_email_no_cc_LCC.txt", "w") do f
+    for (edge, weight) in zip(H_no_cc.elist, H_no_cc.weights)
+        for node in edge
+            # Careful, nodes start from 1, but we want them to start from 0.
+            write(f, string(node - 1), " ")
+        end
+        write(f, string(weight), "\n")
+    end
+end
+
+
+# Create dataset with CC
+kf = true  # Keep fauci
+mindeg = 2
+parts_cc = ("sender", "recipients", "cc")
+H_cc = _build_email_hypergraph(data; hyperedgeparts=parts_cc, keepfauci=kf,  mindegree = mindeg)
+
+open("../Hypergraph_clustering_based_on_PageRank/SubmodularHeatEquation/instance/fauci_email_cc_LCC.txt", "w") do f
+    for (edge, weight) in zip(H_cc.elist, H_cc.weights)
         for node in edge
             # Careful, nodes start from 1, but we want them to start from 0.
             write(f, string(node - 1), " ")
