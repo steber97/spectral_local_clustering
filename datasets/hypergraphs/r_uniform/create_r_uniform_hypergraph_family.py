88388f075776e9b7400868fe09047022cd082d27
Stefano Huber <stefano.huber@gmail.com>
2022-05-30 08:55:42 +0200

    Random r-uniform hypergraph generator takes some random cuts and checks that the conductance is indeed way higher than the target conductance

--- datasets/hypergraphs/r_uniform/create_r_uniform_hypergraph_family.py
+++ datasets/hypergraphs/r_uniform/create_r_uniform_hypergraph_family.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import os
 
 from src.data_structures.hypergraph import HyperGraph, HyperEdge, HyperNode
 
@@ -9,6 +10,9 @@ args_main = argparse.ArgumentParser(
 args_main.add_argument("--conductance", type=float, help='conductance of the family')
 args_main.add_argument("--r", nargs='+', help='list of r values')
 args_main.add_argument('--n', type=int, help='number of nodes')
+args_main.add_argument('--volume', type=int, help='total volume of the hypergraph')
+args_main.add_argument("--out_folder", type=str, help='Output folder for the hypergraph.'
+                                                  'The name of the files will be hypergraph_r_R_vol_VOL_n_N_cond_COND.txt')
 
 args = args_main.parse_args()
 
@@ -25,6 +29,7 @@ if __name__ == "__main__":
     n = args.n
     r_list = [int(x) for x in args.r]
     conductance = args.conductance
+    volume = args.volume
 
     # Create the two bipartitions
     A = [HyperNode(i) for i in range(0, int(n/2))]
@@ -32,8 +37,6 @@ if __name__ == "__main__":
 
 
     for r in r_list: 
-        volume = 10000
-
         # First we add all edges crossing, until we have added a fraction of the volume which is
         #  (1-phi)* volume (we hope that the weight will be distributed approximately 1/2 on both sides)
         hyperedges = []
@@ -49,9 +52,6 @@ if __name__ == "__main__":
                     edge_counter += 1
                     done = True
         hypergraph1 = HyperGraph(hyperedges=hyperedges, hypernodes=hypernodes)
-        print(np.sum(hypergraph1.deg_by_node), 
-              np.sum(hypergraph1.deg_by_node[:int(0.5*n)]), 
-              np.sum(hypergraph1.deg_by_node[int(0.5*n):]))
         volume_so_far = np.sum(hypergraph1.deg_by_node)
         for i in range(int((volume-volume_so_far) /(r * 2))):
             # Add hyperedges inside the two bipartitions
@@ -59,10 +59,46 @@ if __name__ == "__main__":
             hyperedges.append(HyperEdge(np.random.permutation(B)[:r], 1.0, edge_counter + 1))
             edge_counter += 2
         hypergraph2 = HyperGraph(hyperedges=hyperedges, hypernodes=A+B)
-        print(np.sum(hypergraph2.deg_by_node),
-              np.sum(hypergraph2.deg_by_node[:int(0.5*n)]), 
-              np.sum(hypergraph2.deg_by_node[int(0.5*n):]))
-        print(hypergraph2.compute_conductance([i < n/2 for i in range(n)]))
+        print("Total volume: {}, Volume A: {}, Volume B: {}".format(
+            np.sum(hypergraph2.deg_by_node),
+            np.sum(hypergraph2.deg_by_node[:int(0.5*n)]), 
+            np.sum(hypergraph2.deg_by_node[int(0.5*n):])))
+        print("Conductance: {}".format(hypergraph2.compute_conductance([i < n/2 for i in range(n)])))
+
+        # Take some random cuts and check that indeed the conductance for them is high!
+        for i in range(1, 100):
+            cut1, cut2, cut3 = np.zeros(n), np.zeros(n), np.zeros(n)
+            while not((cut1.max() > 0.5 and cut2.max() > 0.5 and cut3.max() > 0.5) and (
+                cut1.min() < 0.5 and cut2.min() < 0.5 and cut3.min() < 0.5)):
+                cut1 = np.array([False if j > n/2 else np.random.rand() > i/(100) for j in range(n)])
+                cut2 = np.array([False if j < n/2 else np.random.rand() > i/(100) for j in range(n)])
+                cut3 = np.array([np.random.rand() > i/(100) for j in range(n)])
+
+            assert hypergraph2.compute_conductance(cut1) > conductance
+            assert hypergraph2.compute_conductance(cut2) > conductance
+            assert hypergraph2.compute_conductance(cut3) > conductance
+
+        # Assert that the graph is connected.
+        assert np.all([len(hypergraph2.adj_list[x]) > 0 for x in hypergraph2.adj_list])
+        
+        filename = "{out_folder}/hypergraph_r_{r}_vol_{vol}_n_{n}_cond_{conductance}.txt".format(
+            out_folder=args.out_folder,
+            r=str(r),
+            vol=str(volume),
+            n=str(n),
+            conductance=str(conductance).replace(".", "_"))  
+        os.makedirs(args.out_folder, exist_ok=True)
+        
+        with open(filename, "w") as f:
+            for he in hypergraph2.hyperedges:
+                row = ""
+                for hn in he.hypernodes:
+                    hn: HyperNode
+                    row += "{} ".format(hn.id)
+                row += "1.0" # The weight of the hyperedge.
+                print(row, file=f)
+        print("Output written to file {}".format(filename))
+
 
 
 
