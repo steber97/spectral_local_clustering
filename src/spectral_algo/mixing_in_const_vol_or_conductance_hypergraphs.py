63c2acecfd4b08140862a9719abc14e1b6306f29
Stefano Huber <stefano.huber@gmail.com>
2022-06-16 11:51:59 +0200

    Code to reproduce plots in experiments for constant conductance and volume

--- src/spectral_algo/mixing_in_const_vol_or_conductance_hypergraphs.py
+++ src/spectral_algo/mixing_in_const_vol_or_conductance_hypergraphs.py
@@ -28,8 +28,22 @@ input_dataset_map = {
     "vol_10000": "datasets/hypergraphs/const_conductance_volume/const_volume/vol_10000"
 }
 
-REPETITIONS = 3
+REPETITIONS = 5
 MU = 0.5  # take cuts as large as 1/2 the volume (no local clustering)
+
+def inv_sq(x: float):
+    return 1/np.array(x)**2
+
+def fit_function(fun, x: np.array, y: np.array) -> float:
+    valmin = np.min(y / fun(x))
+    valmax = np.max(y / fun(x))
+    bestval = valmin
+    i = valmin
+    while i < valmax:
+        if np.sum(np.power(np.abs(y - fun(x) * i), 2)) < np.sum(np.power(np.abs(y - fun(x) * bestval), 2)):
+            bestval = i
+        i += (valmax - valmin) / 100
+    return bestval
 
 if __name__=="__main__":
     iterations = {}
@@ -41,6 +55,10 @@ if __name__=="__main__":
             stationary_distribution = np.array([hypergraph.deg_by_node[n.id] / np.sum(hypergraph.deg_by_node) for n in hypergraph.hypernodes])
             starting_vertices = np.random.permutation(range(len(hypergraph.hypernodes)))
             n = len(hypergraph.hypernodes)
+            print("Volume: {}, Conductance: {}".format(
+                np.sum(hypergraph.deg_by_node),
+                hypergraph.compute_conductance([(True if x < len(hypergraph.hypernodes)//2 else False) for x in range(len(hypergraph.hypernodes))])
+            ))
             for rep in range(REPETITIONS):
                 algo = HyperGraphLocalClusteringDiscrete(hypergraph=hypergraph)
                 # Starting vector centered in one vertex.
@@ -95,9 +113,18 @@ if __name__=="__main__":
     x = [x[0] for x in data]
     y = [x[1] for x in data]
     error = [x[2] for x in data]
-    plt.errorbar(x, y, yerr=error, fmt='-o')
+    plt.errorbar(x, y, yerr=error, fmt='-o' )
     plt.xlabel(xlabel)
     plt.ylabel("iterations")
     plt.title(title)
+    # Plot the fit.
+    # x_fit = np.array([x[0] for x in data])
+    # if "cond" in args.dataset:
+    #     print("Hyperparam: {}".format(fit_function(np.log, x, y)))
+    #     y_fit = np.log(x_fit) * fit_function(np.log, x, y)
+    # else:
+    #     print("Hyperparameter: {}".format(fit_function(inv_sq, x, y)))
+    #     y_fit = inv_sq(x) * fit_function(inv_sq, x, y)
+    # plt.plot(x_fit, y_fit, label="fit")
     # plt.show()
     plt.savefig("{}/{}".format(input_dataset_map[args.dataset], img_name))
