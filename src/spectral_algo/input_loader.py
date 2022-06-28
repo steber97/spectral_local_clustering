039579f7ff0da1911b3a4b5129f28f025a29b750
Stefano Huber <stefano.huber@gmail.com>
2022-06-28 14:05:40 +0200

    Input loader import files only on request

--- src/spectral_algo/input_loader.py
+++ src/spectral_algo/input_loader.py
@@ -28,25 +28,28 @@ input_dataset_map = {
 
 
 def input_loader_hypergraph(dataset_name: str):
-    from datasets.hypergraphs.hypergraph_pagerank_paper.read_hypergraph import read_hypergraph
     if dataset_name == "network_theory":
+        from datasets.hypergraphs.hypergraph_pagerank_paper.read_hypergraph import read_hypergraph
         return read_hypergraph("datasets/hypergraphs/hypergraph_pagerank_paper/network_theory/download.tsv.dimacs10-netscience/dimacs10-netscience/out.dimacs10-netscience",
                                skiprows=1)
     if dataset_name == "opsahl_collaboration":
+        from datasets.hypergraphs.hypergraph_pagerank_paper.read_hypergraph import read_hypergraph
         return read_hypergraph("datasets/hypergraphs/hypergraph_pagerank_paper/opsahl_collaboration/out.opsahl-collaboration",
                                skiprows=2)
     if dataset_name == "dblp_kdd":
+        from datasets.hypergraphs.hypergraph_pagerank_paper.read_hypergraph import read_hypergraph
         return read_hypergraph("datasets/hypergraphs/hypergraph_pagerank_paper/dblp_kdd/out.dblp-kdd",
                                skiprows=2)
     if dataset_name == "dbpedia_writer":
+        from datasets.hypergraphs.hypergraph_pagerank_paper.read_hypergraph import read_hypergraph
         return read_hypergraph("datasets/hypergraphs/hypergraph_pagerank_paper/dbpedia_writer/download.tsv.dbpedia-writer/out.dbpedia-writer",
                                skiprows=2)
-    from datasets.hypergraphs.d_regular_r_uniform.read_graph import read_graph as read_uniform_regular_hypergraph
     if dataset_name == "n_400_d_10_r_8":
+        from datasets.hypergraphs.d_regular_r_uniform.read_graph import read_graph as read_uniform_regular_hypergraph
         return read_uniform_regular_hypergraph("datasets/hypergraphs/d_regular_r_uniform/n_400_d_10_r_8.txt")
 
-    from src.data_structures.hypergraph import HyperGraph
     if dataset_name == "graphprod" or dataset_name == "netscience" or dataset_name == "arxiv":
+        from src.data_structures.hypergraph import HyperGraph
         return HyperGraph.read_hypergraph(input_dataset_map[dataset_name])
 
     if "hypergraph_conductance_0_01_vol_10000_n_100" or "hypergraph_conductance_0_1_vol_10000_n_100" or "hypergraph_conductance_0_1_vol_1000_n_100" in dataset_name:
